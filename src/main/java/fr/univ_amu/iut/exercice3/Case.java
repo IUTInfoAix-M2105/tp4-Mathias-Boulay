d69b8b6bf00e6d7d0e90a33ab1f72d354ed3e042
SerpentSpirale <serpentspirale@gmail.com>
2021-06-02 17:11:35 +0200

    [EX 3] Implement Case class.

--- src/main/java/fr/univ_amu/iut/exercice3/Case.java
+++ src/main/java/fr/univ_amu/iut/exercice3/Case.java
@@ -2,6 +2,7 @@ package fr.univ_amu.iut.exercice3;
 
 
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 class Case extends Button {
@@ -11,21 +12,37 @@ class Case extends Button {
     private ImageView imageView;
 
     public Case(int ligne, int colonne) {
-        throw new RuntimeException("Not yet implemented !");
+        setupImageView();
+        this.ligne = ligne;
+        this.colonne = colonne;
+        setPrefSize(50,50);
+    }
+
+    private void setupImageView(){
+        imageView = new ImageView();
+        imageView.setFitWidth(50);
+        imageView.setPreserveRatio(true);
+        imageView.setSmooth(true);
+        imageView.setCache(true);
     }
 
     public Joueur getPossesseur() {
-        throw new RuntimeException("Not yet implemented !");
+        return possesseur;
     }
 
     public void setPossesseur(Joueur possesseur) {
+        this.possesseur = possesseur;
+    }
+
+    public void setImage(Image image){
+        imageView.setImage(image);
     }
 
     public int getLigne() {
-        throw new RuntimeException("Not yet implemented !");
+        return ligne;
     }
 
     public int getColonne() {
-        throw new RuntimeException("Not yet implemented !");
+        return colonne;
     }
 }
