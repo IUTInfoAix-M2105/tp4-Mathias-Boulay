1152c4861cb4f48f8720d60fbc9d5a3b9271e035
SerpentSpirale <serpentspirale@gmail.com>
2021-06-02 16:21:13 +0200

    [EX 3] Implement statusBar class.

--- src/main/java/fr/univ_amu/iut/exercice3/StatusBar.java
+++ src/main/java/fr/univ_amu/iut/exercice3/StatusBar.java
@@ -1,12 +1,16 @@
 package fr.univ_amu.iut.exercice3;
 
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.When;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -30,28 +34,38 @@ public class StatusBar extends BorderPane implements Initializable {
     private ObjectProperty<Joueur> joueurCourant = new SimpleObjectProperty<>(Joueur.NOIR);
 
     public StatusBar() {
-        throw new RuntimeException("Not yet implemented !");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
+                "/fr/univ_amu/iut/exercice3/StatusBarView.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
     }
 
-    @Override
+    @FXML
     public void initialize(URL location, ResourceBundle resources) {
-        throw new RuntimeException("Not yet implemented !");
     }
 
     private void createBinding() {
-        throw new RuntimeException("Not yet implemented !");
+        messageScoreNoir.textProperty().bind(Bindings.format(SCORE_NOIR + "%d", Joueur.NOIR.scoreProperty()));
+        messageScoreBlanc.textProperty().bind(Bindings.format(SCORE_BLANC + "%d", Joueur.BLANC.scoreProperty()));
+        messageTourDeJeu.textProperty().bind(Bindings.when(Bindings.equal(Joueur.BLANC, joueurCourant)).then(MESSAGE_TOUR_BLANC).otherwise(MESSAGE_TOUR_NOIR));
     }
 
     public Joueur getJoueurCourant() {
-        throw new RuntimeException("Not yet implemented !");
+        return joueurCourant.get();
     }
 
     public void setJoueurCourant(Joueur joueurCourant) {
-        throw new RuntimeException("Not yet implemented !");
+        this.joueurCourant.setValue(joueurCourant);
     }
 
     public ObjectProperty<Joueur> joueurCourantProperty() {
-        throw new RuntimeException("Not yet implemented !");
+        return joueurCourant;
     }
 
 
